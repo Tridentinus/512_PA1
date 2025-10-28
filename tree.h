5ea1002ca3aa2ea7677ef522fca3a961bb0b33af
Trent Michael Seaman <seaman7@eceprog4.ecn.purdue.edu>
2025-10-28 13:08:02 -0400

    cleaned

--- tree.h
+++ tree.h
@@ -23,32 +23,19 @@ void recur_delay(Node * node, double pDelay, double pResistance,double r,FILE* o
 void dp_delay (Node * root, double Rb,double re,FILE * out);
 
 
-double compute_max_distance_hyp(double CT, double Tmax, double T_constraint, 
-                                double Rb, double r, double c, double Co);
+double compute_max_distance_hyp(double CT, double Tmax, double T_constraint, double Rb, double r, double c, double Co);
 
-Node* insert_inverter(Node* node, double L_max, double Rb, double r, double c, 
-                      double Co, double Cb, double Tb);
+Node* insert_inverter(Node* node, double L_max, double Rb, double r, double c,  double Co, double Cb, double Tb);
 
 
-Node* insert_inverter_on_child(Node* parent,int child_side,
-                                double Rb, double r, double c, 
-                                double Co, double Cb, double Tb);
+Node* insert_inverter_on_child(Node* parent,int child_side, double Rb, double r, double c, double Co, double Cb, double Tb);
 
-bool process_node(Node* node, double T_constraint, 
-                  double Rb, double r, double c, double Co, double Cb, double Tb);
+bool process_node(Node* node, double T_constraint,double Rb, double r, double c, double Co, double Cb, double Tb);
 
-bool inverter_insertion(Node* root, double T_constraint, 
-                        double Rb, double r, double c, double Co, double Cb, double Tb);
+bool inverter_insertion(Node* root, double T_constraint, double Rb, double r, double c, double Co, double Cb, double Tb);
 
 void postorder_with_inverters(Node* root, FILE* out);
 void postorder_with_inverters_binary(Node* root, FILE* out);
 
 
-void count_sinks(Node* node, std::set<int>& sinks);
-void count_edges(Node* node, std::set<std::pair<int, int>>& edges, int parent_id);
-void check_parity(Node* node, int& noninverting_count);
-void count_inverters(Node* node, int& inverter_count, bool& all_valid, bool count_root );
-void count_stage_sinks(Node* node, double T_constraint, int& stage_sinks, int& safe_stage_sinks);
-void verify_solution(Node* original_root, Node* modified_root, double T_constraint, 
-                     const char* test_name);
 
